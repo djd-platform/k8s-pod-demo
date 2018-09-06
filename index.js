24560283cb875334b4349aebb92b196f7ffe7cf7
Oliver Diamond <oliver.diamond@sky.uk>
2018-09-06 15:09:12 +0100

    Logging visits

--- index.js
+++ index.js
@@ -2,18 +2,37 @@ const http = require('http');
 const uuid = require('uuid/v4')();
 const version = 'version 1';
 
-const html = `
-<html>
-  <body style="background-color: #faa;">
-    <p>Version: ${version}</p>
-    <p>UUID: ${uuid}</p>
-  </body>
-</html>
-`
+const visitHistory = [];
+
+const html = visitHistory => {
+  const visitHistoryTable = visitHistory.map(({time, address, userAgent}) => `<br />${time}: ${address} (${userAgent})`).join('')
+
+  return `
+  <html>
+    <head>
+      <title>K8s Pod Demo</title>
+    </head>
+    <body style="background-color: #faa;">
+      <p>Version: ${version}</p>
+      <p>UUID: ${uuid}</p>
+      <p>Visits: ${visitHistoryTable}</p>
+    </body>
+  </html>
+  `;
+}
+
 const server = http.createServer((request, response) => {
+  if(request.url === '/'){
+    const visit = {
+      time: (new Date()).toLocaleString(),
+      address: request.connection.remoteAddress,
+      userAgent: request.headers['user-agent'],
+    }
+    console.log(visit)
+    visitHistory.push(visit);
+  }
   response.writeHead(200, {'Content-type':'text/html'});
-  response.write(html);
-  response.end();
+  response.end(html(visitHistory));
 });
 
 server.listen(4000, () => console.log('listening'))
